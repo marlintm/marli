77eff37eff9c006813a9ab5d376b1445acfb5bf7
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-05 19:03:44 +0200

    Update thumbnails.py

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -60,7 +60,7 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
     image3 = Image.fromarray(final_img_arr)
     image3 = image3.resize((500,500))
                              
-    image2.paste(image3, (60,168), mask = image3)
+    image2.paste(image3, (80,172), mask = image3)
     image2.paste(circle, (0,0), mask = circle)
 
     # fonts
