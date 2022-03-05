d0bca6899966a4c53d9cc7430896d05379de725d
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-05 19:15:53 +0200

    Update thumbnails.py

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -53,7 +53,7 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
     image3 = image11.crop((280,0,1000,720))
     lum_img = Image.new('L', [720,720] , 0)
     draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (720,720)], 0, 300, fill = 300, outline = "white")
+    draw.pieslice([(0,0), (720,720)], 0, 100, fill = 100, outline = "white")
     img_arr =np.array(image3)
     lum_img_arr =np.array(lum_img)
     final_img_arr = np.dstack((img_arr,lum_img_arr))
