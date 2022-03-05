b414f011d004918ec50e3f5fbcb33c151a110bd3
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-05 19:27:56 +0200

    gnjvgn

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -53,14 +53,14 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
     image3 = image11.crop((280,0,1000,720))
     lum_img = Image.new('L', [720,720] , 0)
     draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (500,500)], 0, 360, fill = 255, outline = "white"
+    draw.pieslice([(200,200), (720,720)], 0, 360, fill = 255, outline = "white")
     img_arr =np.array(image3)
     lum_img_arr =np.array(lum_img)
     final_img_arr = np.dstack((img_arr,lum_img_arr))
     image3 = Image.fromarray(final_img_arr)
-    image3 = image3.resize((600,600))
-                             
-    image2.paste(image3, (92,180), mask = image3)
+    image3 = image3.resize((500,500))
+
+    image2.paste(image3, (90,183), mask = image3)
     image2.paste(circle, (0,0), mask = circle)
 
     # fonts
