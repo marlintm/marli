965ee9ced18d1041520295e8d87a3b5b39a725ec
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-15 13:41:37 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -73,7 +73,6 @@ async def play_commnd(
     plist_type = None
     spotify = None
     user_id = message.from_user.id
-    if await check_is_joined(userid)
     user_name = message.from_user.first_name
     audio_telegram = (
         (
